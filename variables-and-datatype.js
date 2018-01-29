7f3f34e313b5208874126f3ea362a5145ab58069
Romulus <romulus.senter@yahoo.com>
2018-01-29 18:55:13 -0500

    learned how to create a var

---
+++ variables-and-datatype.js
@@ -0,0 +1,11 @@
+//Single line comment
+/** This is  multi line comment
+ */
+
+ //Variables
+ var name = "Romulus"; //This is old school javascript (dont use this)
+ const fruit = "Banana"; // Constant binding (lock it in the cup)
+ let veggie = "carrot"; //simmilar to var, but some otherdifferences. 
+console.log ("Name: ", name); //print
+console.log ("Fruit: ", fruit);
+console.log ("Veggie: ", veggie); 
