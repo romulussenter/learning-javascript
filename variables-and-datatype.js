91d31c2e940c42984f10350c1e47a204bd12dc16
Romulus <romulus.senter@yahoo.com>
2018-01-29 19:36:08 -0500

    looked as strings and numbers

--- variables-and-datatype.js
+++ variables-and-datatype.js
@@ -9,3 +9,25 @@
 console.log ("Name: ", name); //print
 console.log ("Fruit: ", fruit);
 console.log ("Veggie: ", veggie); 
+
+
+// Simple Datatypes
+//Strings
+const aString = "things and stuff";
+//a string => spadce in variables names are .. bad!!
+//so wee remove the soace and capitalize all words but the first.
+// this is a called camel casing CONVENTION!!!
+const anotherString = 'some other things and stuff'; //this is a single quotes
+const stringWithNumbers = '1919192';
+console.log("aString: ", aString);
+console.log("anotherString: ", anotherString);
+console.log("stringWithNumber: ", stringWithNumbers);
+console.log("What is the type of aString?", typeof aString);
+// String will come back up. There is more to know 
+//Numbers 
+const aNumber = 1;
+const anotherNumber = 3;
+console.log("aNumber: " , aNumber);
+console.log("anotherNumber:" ,anotherNumber);
+console.log("Type of aNumber: " ,typeof aNumber);
+console.log("Type of anotherNumber: ", typeof anotherNumber);
